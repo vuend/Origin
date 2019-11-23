57c0f4bc402f75ab03bcb4673dbfd05f99620835
vuend <57929455+vuend@users.noreply.github.com>
2019-11-23 16:33:28 +0800

    Add files via upload

---
+++ src/Class/HW2.java
@@ -0,0 +1,9 @@
+package Class;
+
+public class HW2 {
+    public static void main(String[] args) {
+        //For Loop
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World");
+    }
+}
